608c82ce5f56e5a4d839da5b1146f2de52290792
Udant <udantkandarpa@hotmail.com>
2020-04-15 17:29:11 +0530

    Class 37 project

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var gameState = 0;
+var playerCount;
+var database;
+var game;
+var player;
+var form;
+var allPlayers;
+var cars,car1,car2,car3,car4;
+function setup(){
+    canvas = createCanvas(displayWidth-20,displayHeight - 30);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    if(playerCount === 4){
+        game.update(1);
+      } 
+      if(gameState === 1){
+        clear();
+        game.play();
+      } 
+      
+}
+
